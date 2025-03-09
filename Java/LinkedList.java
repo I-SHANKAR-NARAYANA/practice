321f0f520e0d89cffe685088ee87b00279615324
ISN <shankara.vadhulasa@gmail.com>
2025-03-09 10:00:00 +0530

    Add Java LinkedList with append and print operations

---
+++ Java/LinkedList.java
@@ -0,0 +1,32 @@
+public class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) { data = d; next = null; }
+    }
+
+    Node head;
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) { head = newNode; return; }
+        Node last = head;
+        while (last.next != null) last = last.next;
+        last.next = newNode;
+    }
+
+    public void printList() {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.append(1); ll.append(2); ll.append(3);
+        ll.printList();
+    }
+}
