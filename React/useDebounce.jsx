5469db72e64453c1e434bfd8aec55b6c099121b1
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2025-07-08 04:17:11 +0530

    minor: cleanup in useDebounce.jsx

--- React/useDebounce.jsx
+++ React/useDebounce.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 function useDebounce(value, delay = 500) {
+
   const [debounced, setDebounced] = useState(value);
   useEffect(() => {
     const timer = setTimeout(() => setDebounced(value), delay);
