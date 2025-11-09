f0f177420d2326fa6f5038c7524d00b1e04340b2
ISN <shankara.vadhulasa@gmail.com>
2025-11-09 10:00:00 +0530

    Add GraphQL schema with User, Post types and mutations

---
+++ GraphQL/schema.js
@@ -0,0 +1,39 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    posts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    author: User!
+    tags: [String!]!
+    createdAt: String!
+  }
+
+  type Query {
+    user(id: ID!): User
+    users: [User!]!
+    post(id: ID!): Post
+    posts(tag: String): [Post!]!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
+    createPost(
+      title: String!
+      content: String!
+      authorId: ID!
+      tags: [String!]
+    ): Post!
+    deletePost(id: ID!): Boolean!
+  }
+`;
+
+module.exports = typeDefs;
