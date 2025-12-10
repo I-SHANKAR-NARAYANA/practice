c21e55928f09d6fbbfd39d495f28320c2cad2289
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2025-12-10 17:14:46 +0530

    style: whitespace in middleware.js

--- Express/middleware.js
+++ Express/middleware.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
 const logger = (req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
